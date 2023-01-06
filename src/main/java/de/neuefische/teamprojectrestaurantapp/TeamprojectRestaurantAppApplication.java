146a9f6d87eeb2258dcffc8fbe5e1c69ea63ecc2
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-01-06 10:11:31 +0100

    initial commit

---
+++ src/main/java/de/neuefische/teamprojectrestaurantapp/TeamprojectRestaurantAppApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.teamprojectrestaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectRestaurantAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamprojectRestaurantAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/teamprojectrestaurantapp/TeamprojectRestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.teamprojectrestaurantapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectRestaurantAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
