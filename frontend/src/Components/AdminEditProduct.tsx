cce080de77d277bead0caff9d75f25dc7b46fa58
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-01-10 09:44:58 +0100

    Connected Frontend with Backend

--- frontend/src/Components/AdminEditProduct.tsx
+++ frontend/src/Components/AdminEditProduct.tsx
@@ -1,15 +1,7 @@
 export default function AdminEditProduct() {
     return (
         <>
-            <h1>Edit Products</h1>
 
-            <ul>
-                {products.length < 1 ?
-                <h1>Keine Produkte vorhanden...</h1> :
-                    products.map((product) =>
-                    <li key={product.id}>{product.name}</li>)
-                }
-            </ul>
         </>
     )
 }
---
+++ frontend/src/Components/Product.tsx
@@ -0,0 +1,11 @@
+export type Product = {
+    id: string;
+    name: string;
+    image: string;
+    price: number;
+    description: string;
+    category: string;
+    drinkSize: number;
+    spicyScale: number;
+    quantity: number;
+}
