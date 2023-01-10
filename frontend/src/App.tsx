e4043b950d86d23066bd4c82c935591de83a212b
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-01-10 09:44:58 +0100

    Connected Frontend with Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import AdminDashboard from "./Pages/AdminDashboard"
+import {Product} from "./Components/Product";
+import axios from "axios";
+import AdminAddProduct from "./Components/AdminAddProduct";
 
 function App() {
+    const [products, setProducts] = useState<Product[]>([])
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get("/api/restaurant")
+            setProducts(response.data);
+        })();
+    }, []);
+
   return (
-        <AdminDashboard/>
+      <>
+          <AdminAddProduct/>
+          {products.length <= 1 ?
+              <p>Keine Producte vorhanden...</p> :
+              <div>Platzhalter für ProductList</div>
+          }
+      </>
   );
 }
 
