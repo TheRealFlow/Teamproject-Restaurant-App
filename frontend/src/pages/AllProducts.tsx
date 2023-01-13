fe98276b943ee1465ce14879ac6926a895129538
Lmes98 <118824069+Lmes98@users.noreply.github.com>
2023-01-13 10:34:36 +0100

    routing added

---
+++ frontend/src/pages/AllProducts.tsx
@@ -0,0 +1,13 @@
+import ProductList from "../Components/ProductList";
+import './style.css';
+
+export function AllProducts(){
+
+    return(
+
+        <div className={"distanceTop"}>
+            <ProductList />
+        </div>
+
+    )
+}
